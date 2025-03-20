d9bf271c696bd36cf8aa5dd7af812d9f57da8930
Vasyl Kogotyuk <vasyakohotyk@gmail.com>
2025-03-20 21:20:19 +0200

    increased the distance between blocks

--- src/InteractiveWorkSpace/utils/constants.ts
+++ src/InteractiveWorkSpace/utils/constants.ts
@@ -4,8 +4,8 @@ export const defaultBlocks: Block[] = [
   { id: "1", x: 10, y: 10, width: 300, height: 100, color: "#ffadad", zIndex: 1 },
   { id: "2", x: 400, y: 10, width: 300, height: 100, color: "#ffd6a5", zIndex: 2 },
   { id: "3", x: 800, y: 10, width: 300, height: 100, color: "#fdffb6", zIndex: 3 },
-  { id: "4", x: 10, y: 150, width: 300, height: 100, color: "#caffbf", zIndex: 4 },
-  { id: "5", x: 400, y: 150, width: 300, height: 100, color: "#9bf6ff", zIndex: 5 },
+  { id: "4", x: 10, y: 200, width: 300, height: 100, color: "#caffbf", zIndex: 4 },
+  { id: "5", x: 400, y: 200, width: 300, height: 100, color: "#9bf6ff", zIndex: 5 },
 ];
 
 
